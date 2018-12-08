7f60e1a110b2cb3c6c3d0f55078870587f54250e
Arnaud Rouanet <arnaud@rouanet.org>
2018-12-08 23:27:10 +0100

    Add .dir-locals.el file

---
+++ .dir-locals.el
@@ -0,0 +1,8 @@
+((nil
+  (indent-tabs-mode . nil))
+ (emacs-lisp-mode
+  (fill-column . 72)
+  (sentence-end-double-space . t))
+ (markdown-mode
+  (fill-column . 80)
+  (sentence-end-double-space . nil)))
